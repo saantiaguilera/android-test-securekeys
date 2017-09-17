117979173ffafd9d6b84ca7ab60789a82cff60fc
Santi Aguilera <aguilerasanti@hotmail.com>
2017-09-16 23:51:02 -0300

    Update ndk install script [circleci]

--- ci/install_ndk.sh
+++ ci/install_ndk.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-NDK_VERSION=r14
-TMP_NDK=/tmp/ndk
-DIR=android-ndk-$NDK_VERSION
-FILE=$DIR-linux-x86_64.bin
-
-mkdir -p $TMP_NDK
-curl -L -o $TMP_NDK/$FILE https://dl.google.com/android/ndk/$FILE
-
-pushd $TMP_NDK
-chmod a+x $FILE
-./$FILE
-mv $DIR /opt/android/sdk/ndk-bundle
-popd
-
-export ANDROID_NDK=/opt/android/sdk/ndk-bundle
-
-export PATH="$PATH:$ANDROID_NDK"
-export ANDROID_NDK_HOME="$ANDROID_NDK"
-
-rm -rf $TMP_NDK
+if [[ ! -e ~/android-ndk-$1 ]]; then
+	wget http://dl.google.com/android/repository/android-ndk-$1-linux-x86_64.zip
+	unzip -qq -d ~ android-ndk-$1-linux-x86_64.zip
+fi
