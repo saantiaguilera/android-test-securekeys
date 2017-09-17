3f312d447a94fbbaa2791ff76cfa358e15764b88
Santi Aguilera <aguilerasanti@hotmail.com>
2017-09-16 23:23:58 -0300

    Install NDK for 2.0 cli [circleci]

--- .circleci/install_ndk.sh
+++ .circleci/install_ndk.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-if [[ ! -e ~/android-ndk-$1 ]]; then
-	wget http://dl.google.com/android/repository/android-ndk-$1-linux-x86_64.zip
-	unzip -qq -d ~ android-ndk-$1-linux-x86_64.zip
-fi
+NDK_VERSION=r14
+TMP_NDK=/tmp/ndk
+DIR=android-ndk-$NDK_VERSION
+FILE=$DIR-linux-x86_64.bin
+
+mkdir -p $TMP_NDK
+curl -L -o $TMP_NDK/$FILE https://dl.google.com/android/ndk/$FILE
+
+pushd $TMP_NDK
+chmod a+x $FILE
+./$FILE
+mv $DIR /usr/local/android-ndk
+popd
+
+export ANDROID_NDK=/usr/local/android-ndk
+
+export PATH="$PATH:$ANDROID_NDK"
+export ANDROID_NDK_HOME="$ANDROID_NDK"
+
+rm -rf $TMP_NDK
